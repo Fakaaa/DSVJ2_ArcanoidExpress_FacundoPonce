98f42081e37a97c36b049d27d9aaee39013f261a
FacundoPonce <mrfacu125@gmail.com>
2021-05-30 19:11:37 -0300

    Player lifes working.

--- ArcanoidExpress!/Assets/Scirpts/Player.cs
+++ ArcanoidExpress!/Assets/Scirpts/Player.cs
@@ -9,8 +9,29 @@ public class Player : MonoBehaviour
     [SerializeField] private int limitMin;
 
     private Vector3 moveVec;
-    
 
+    [SerializeField] private int playerLifes;
+    [SerializeField] public bool isAlive;
+
+    private void Start()
+    {
+        isAlive = true;
+        TriggerLostBall.playerLostBall += RestLife;
+    }
+    private void OnDisable()
+    {
+        TriggerLostBall.playerLostBall -= RestLife;
+    }
+    public void RestLife()
+    {
+        playerLifes--;
+
+        if(playerLifes <= 0)
+        {
+            playerLifes = 0;
+            isAlive = false;
+        }
+    }
     void MovePlayer()
     {
         moveVec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
---
+++ ArcanoidExpress!/Assets/Scirpts/TriggerLostBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLostBall : MonoBehaviour
+{
+    public delegate void PlayerHasLostTheBall();
+    public static PlayerHasLostTheBall playerLostBall;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Ball")
+        {
+            playerLostBall?.Invoke();
+            Debug.Log("Entro");
+        }
+    }
+}
