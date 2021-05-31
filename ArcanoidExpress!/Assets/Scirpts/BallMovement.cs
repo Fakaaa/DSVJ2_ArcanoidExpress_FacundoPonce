ac791cf2018284dc447bc9777c6a4f3b9c3fa6dd
FacundoPonce <mrfacu125@gmail.com>
2021-05-31 20:12:33 -0300

    All collisions working perfectly.

--- ArcanoidExpress!/Assets/Scirpts/BallMovement.cs
+++ ArcanoidExpress!/Assets/Scirpts/BallMovement.cs
@@ -8,9 +8,19 @@ public class BallMovement : MonoBehaviour
 
     [SerializeField] private Transform playerTransform;
     [SerializeField] private Transform startPos;
+    [SerializeField] private Vector3 directionBall;
+    [SerializeField] private float speedBall;
+
+    [SerializeField] private float lenghtRaycast;
+    private Vector3 initialScale;
+
+    private Ray rayFromBall;
+    private RaycastHit hitBall;
 
     void Start()
     {
+        initialScale = transform.localScale;
+        directionBall = transform.forward;
         startPos.position = transform.position;
         transform.parent = playerTransform;
         Player.playerReleasesBall += ReleaseBall;
@@ -30,11 +40,49 @@ public class BallMovement : MonoBehaviour
     void AttachBallToPlayer()
     {
         isMoving = false;
-        transform.parent = playerTransform;
         transform.position = startPos.position;
+        transform.parent = playerTransform;
+        transform.localScale = initialScale;
+        directionBall = transform.forward;
     }
-    //void Update()
-    //{
-    //    
-    //}
+    private void UpdateRay()
+    {
+        rayFromBall = new Ray(transform.position, transform.forward * lenghtRaycast);
+        Debug.DrawRay(rayFromBall.origin, rayFromBall.direction * lenghtRaycast, Color.red);
+    }
+    void MoveBall()
+    {
+        transform.position += directionBall * speedBall * Time.deltaTime;
+    }
+    void Update()
+    {
+        if (isMoving)
+            MoveBall();
+
+        RaycastCheckCollision();
+
+        UpdateRay();
+    }
+    void RaycastCheckCollision()
+    {
+        if(Physics.Raycast(rayFromBall, out hitBall, lenghtRaycast))
+        {
+           
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Vector3 resultDirection = transform.position - collision.collider.gameObject.transform.position;
+            directionBall = resultDirection.normalized;
+        }
+        else
+        {
+            if(collision.collider.CompareTag("Wall"))
+                directionBall = new Vector3(-directionBall.x, directionBall.y, directionBall.z);
+            else
+                directionBall = new Vector3(directionBall.x, directionBall.y, -directionBall.z);
+        }
+    }
 }
